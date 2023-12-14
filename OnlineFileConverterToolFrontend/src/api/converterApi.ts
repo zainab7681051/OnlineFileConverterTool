330e91e4e7b941c42de51f7acf0eba8ce1c65995
zainab7681051 <zainab76810510@gmail.com>
2023-12-15 00:28:38 +0300

    commit

--- OnlineFileConverterToolFrontend/src/api/converterApi.ts
+++ OnlineFileConverterToolFrontend/src/api/converterApi.ts
@@ -1,16 +1,21 @@
 const baseUrl: string = import.meta.env.VITE_BASE_URL;
 const tag: string = "Upload";
 
-export async function Convert(file: File, fromFormat: string, toFormat: string): Promise<Response> {
-    const formData = new FormData();
-    formData.append('file', file)
+export async function Convert(file: File, fromFormat: string, toFormat: string): Promise<Response | null> {
+    try {
+        const formData = new FormData();
+        formData.append('file', file)
 
-    const requestOptions: RequestInit = {
-        method: 'POST',
-        body: formData,
-        mode: 'cors',
-    };
-    const param = "from=" + fromFormat + "&to=" + toFormat;
-    const url = baseUrl + '/' + tag + '?' + param;
-    return await fetch(url, requestOptions);
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            body: formData,
+            mode: 'cors',
+        };
+        const param = "from=" + fromFormat + "&to=" + toFormat;
+        const url = baseUrl + '/' + tag + '?' + param;
+        return await fetch(url, requestOptions);
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
